f7438895015b8dc39e9b05c0ff78a2a112887195
Jiří Kanda <kanda@havit.cz>
2025-09-26 15:41:23 +0200

    HavitAzureSqlDatabasesExport - refactoring, retry logic

--- HavitAzureSqlDatabasesExport/HavitAzureSqlDatabasesExportCore.psm1
+++ HavitAzureSqlDatabasesExport/HavitAzureSqlDatabasesExportCore.psm1
@@ -1,4 +1,7 @@
-﻿function Export-AzureSqlDatabases
+﻿$sleepDurationSeconds = 15
+$waitSingleAzureSqlDatabaseExportCompletionMinutes = 120
+
+function Export-AzureSqlDatabases
 {
     param($DbServerName,
         $DbServerResourceGroupName,
@@ -10,9 +13,6 @@
         $StorageKey,
         $StorageResourceIdForPrivateLink
     )
-
-    # PREPARE storageUri
-    $storageUri = "https://$StorageAccountName.blob.core.windows.net/$StorageContainerName/" # musí končit limítkem
 
     #REMOVE PREVIOUS BLOBS
 
@@ -34,147 +34,214 @@
     $exportFailed = $false
     
     foreach ($database in $databases)
-    {           
+    { 
         if ($database.DatabaseName -match '_d{4}-\d{2}-\d{2}T\d{2}-\d{2}Z$')
         {
             Write-Host "Skipping export $($database.DatabaseName) database."
         }
-
-        #using dbServerName as prefix (upper case) (to distinquish database sources and to be able to clear only database exports in storage)
-        $bacpacName = $DbServerName.ToUpper() + '-' + $database.DatabaseName + '.bacpac'
-        $storageBlobUri = $StorageUri + $bacpacName
-    
-        Write-Host "Starting export $($database.DatabaseName) database to $bacpacName..."
         
-        $PSPersistPreference = $True
-
-        $exportRequestParams = @{
-            ResourceGroupName                   = $DbServerResourceGroupName
-            ServerName                          = $DbServerName.ToLower()
-            DatabaseName                        = $database.DatabaseName
-            StorageKeyType                      = "StorageAccessKey"
-            StorageKey                          = $StorageKey
-            StorageUri                          = $storageBlobUri
-            UseNetworkIsolation                 = $true
-            AdministratorLogin                  = $DbServerAdministratorUsername
-            AdministratorLoginPassword          = $DbServerAdministratorPassword
-            StorageAccountResourceIdForPrivateLink = $StorageResourceIdForPrivateLink
-            SqlServerResourceIdForPrivateLink   = $DbServerResourceIdForPrivateLink
-        }
-
-        $exportRequest = New-AzSqlDatabaseExport @exportRequestParams
-
-        if (-not $exportRequest)
+        try
         {
-            Write-Warning "Failed."
-            Write-Host "Waiting 120 seconds..."
-            Start-Sleep -Second 120
-
-            Write-Host "Starting export $($database.DatabaseName) database to $bacpacName..."
-            $exportRequest = New-AzSqlDatabaseExport @exportRequestParams
+            Export-SingleAzureSqlDatabase -database $database
         }
-        
-        $PSPersistPreference = $false
-
-        if ($exportRequest)
+        catch
         {
-            Write-Host "Started export $($database.DatabaseName) database to $($exportRequest.StorageUri)."
-            
-            # PRIVATE LINK ACCEPTANCE
-            if ($dbServerResourceIdForPrivateLink)
-            {
-                $counter = 1
-                $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink                
-                while ($exportRequestStatus.PrivateEndpointRequestStatus.Count -eq 0)
-                {
-                    if ($counter -ge 12)
-                    {
-                        Write-Error "Private link information not retrieved."
-                        $exportFailed = $true
-                        break
-                    }
-                    $counter += 1
-                    Write-Host "Waiting for private link connection info..."
-                    Start-Sleep -Seconds 15
-                    $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink
-                }
-
-                $counter = 1
-                $sqlServerPrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $DbServerResourceIdForPrivateLink | where { $_.Name.StartsWith('ImportExportPrivateLink_SQL') } | where {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
-                while (!$sqlServerPrivateEndpointConnection)
-                {
-                    if ($counter -ge 12)
-                    {
-                        Write-Error "Azure SQL Server private link not available."
-                        $exportFailed = $true
-                        break
-                    }
-                    $counter += 1
-                    Write-Host "Waiting for Azure SQL Server private link connection info..."
-                    Start-Sleep -Seconds 15
-                    $sqlServerPrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $DbServerResourceIdForPrivateLink | where { $_.Name.StartsWith('ImportExportPrivateLink_SQL') } | where {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
-                }
-
-                $counter = 1
-                $storagePrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $StorageResourceIdForPrivateLink | where {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
-                while (!$storagePrivateEndpointConnection)
-                {
-                    if ($counter -ge 12)
-                    {
-                        Write-Error "Azure Storage private link not available."
-                        $exportFailed = $true
-                        break
-                    }
-                    $counter += 1
-                    Write-Host "Waiting for Storage private link connection info..."
-                    Start-Sleep -Seconds 15
-                    $storagePrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $StorageResourceIdForPrivateLink | where {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
-                }
-
-                if ($sqlServerPrivateEndpointConnection)
-                {
-                    Write-Host "Accepting SQL Server private link..."
-                    Approve-AzPrivateEndpointConnection -ResourceId $sqlServerPrivateEndpointConnection.Id
-                }
-
-                if ($storagePrivateEndpointConnection)
-                {
-                    Write-Host "Accepting Storage private link..."
-                    Approve-AzPrivateEndpointConnection -ResourceId $storagePrivateEndpointConnection.Id
-                }
-            }
-
-            # ELASTIC POOL - OVERLOAD PROTECTION
-            # export databázi nemálo vytíží
-            # tam, kde není elastic pool, s tím mnoho nenaděláme (můžeme povýšit), prostě musíme chvíli exportu přežít
-            # tam, kde je elastic pool, můžeme *nezpůsobit* zátěž paralelní zálohou všech databází, čímž by měl zůstat výkový prostor pro běh aplikací
-            if ($database.ElasticPoolName) # not null, not empty
-            {
-                $counter = 0
-                do
-                {
-                    $counter += 1
-                    if ($counter -eq 480) # 120 minut
-                    {
-                        Write-Warning "Timeout reached during database $($exportRequest.DatabaseName) export."
-                        break
-                    }
-                    Start-Sleep -Second 15 # pokud by se změnilo, pozor na podmínku ukončení cyklu z důvodu timeoutu
-                    $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink
-                    Write-Host "  $($counter): Database $($database.DatabaseName) export status is $($exportRequestStatus.Status) ($($exportRequestStatus.StatusMessage))."                        
-                } while ($exportRequestStatus.Status -eq "InProgress")
-            }
+            Write-Error "Caught: $($_.Exception.Message)"
+            $exportFailed = $true
         }
-        else
-        {
-            $exportFailed = $true;
-            Write-Error "Export $($database.DatabaseName) database failed."
-        }      
     }
     
     if ($exportFailed -eq $true)
     {
         throw "Database export failed."
     }
+}
 
+
+function Export-SingleAzureSqlDatabase
+{
+    param (
+        [Microsoft.Azure.Commands.Sql.Database.Model.AzureSqlDatabaseModel] $database,
+        [int] $depth = 0
+    )
+
+    try
+    {
+        $exportRequest = Start-SingleAzureSqlDatabaseExport -database $database
+        # PRIVATE LINK ACCEPTANCE
+        Accept-SingleSqlDatabaseExportPrivateLinkEndpoints -database $database
+
+        # ELASTIC POOL - OVERLOAD PROTECTION
+        # export databázi nemálo vytíží
+        # tam, kde není elastic pool, s tím mnoho nenaděláme (můžeme povýšit), prostě musíme chvíli exportu přežít
+        # tam, kde je elastic pool, můžeme *nezpůsobit* zátěž paralelní zálohou všech databází, čímž by měl zůstat výkový prostor pro běh aplikací        
+        if ($database.ElasticPoolName) # not null, not empty
+        {        
+            Wait-SingleAzureSqlDatabaseExportCompletion -exportRequest $exportRequest
+        }
+    }
+    catch [RetryException]
+    {        
+        Write-Error $exception.Message
+        Write-Host "Waiting 10 minutes..."
+        Start-Sleep -Seconds 600 # 10 minutes
+        if ($depth -lt 5) # max attempts per database
+        {
+        }
+        else
+        {
+            throw $_.Exception
+        }
+    }
+}
+
+function Start-SingleAzureSqlDatabaseExport
+{
+    param (
+        [Microsoft.Azure.Commands.Sql.Database.Model.AzureSqlDatabaseModel] $database
+    )
+
+    $storageUri = "https://$StorageAccountName.blob.core.windows.net/$StorageContainerName/" # musí končit limítkem
+    $bacpacName = $DbServerName.ToUpper() + '-' + $database.DatabaseName + '.bacpac'
+    $storageBlobUri = $StorageUri + $bacpacName
+
+    $exportRequestParams = @{
+        ResourceGroupName                   = $DbServerResourceGroupName
+        ServerName                          = $DbServerName.ToLower()
+        DatabaseName                        = $database.DatabaseName
+        StorageKeyType                      = "StorageAccessKey"
+        StorageKey                          = $StorageKey
+        StorageUri                          = $storageBlobUri
+        UseNetworkIsolation                 = $true
+        AdministratorLogin                  = $DbServerAdministratorUsername
+        AdministratorLoginPassword          = $DbServerAdministratorPassword
+        StorageAccountResourceIdForPrivateLink = $StorageResourceIdForPrivateLink
+        SqlServerResourceIdForPrivateLink   = $DbServerResourceIdForPrivateLink
+    }
+
+    Write-Host "Starting export $($database.DatabaseName) database to $bacpacName..."
+
+    $exportRequest = New-AzSqlDatabaseExport @exportRequestParams
+
+    if (-not $exportRequest)
+    {
+        Write-Warning "Failed."
+        Write-Host "Waiting 120 seconds..."
+        Start-Sleep -Second 120
+
+        Write-Host "Starting export $($database.DatabaseName) database to $bacpacName..."
+        $exportRequest = New-AzSqlDatabaseExport @exportRequestParams
+    }
+
+    if ($exportRequest)
+    {
+        Write-Host "Started export $($database.DatabaseName) database to $($exportRequest.StorageUri)."
+    }
+    else
+    {
+        throw "Starting export $($database.DatabaseName) failed."
+    }
+
+    return $exportRequest
+}
+
+function Accept-SingleSqlDatabaseExportPrivateLinkEndpoints
+{
+    param (
+        [Microsoft.Azure.Commands.Sql.ImportExport.Model.AzureSqlDatabaseImportExportBaseModel] $exportRequest
+    )
+
+    if ($dbServerResourceIdForPrivateLink)
+    {
+        $counter = 1
+        $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink                
+        while ($exportRequestStatus.PrivateEndpointRequestStatus.Count -eq 0)
+        {
+            if ($counter -ge 12)
+            {
+                throw "Private link information not retrieved."
+            }
+            $counter += 1
+            Write-Host "Waiting for private link connection info..."
+            Start-Sleep -Seconds 15
+            $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink
+        }
+
+        $counter = 1
+        $sqlServerPrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $DbServerResourceIdForPrivateLink | Where-Object { $_.Name.StartsWith('ImportExportPrivateLink_SQL') } | Where-Object {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
+        while (!$sqlServerPrivateEndpointConnection)
+        {
+            if ($counter -ge 12)
+            {
+                throw "Azure SQL Server private link not available."
+            }
+            $counter += 1
+            Write-Host "Waiting for Azure SQL Server private link connection info..."
+            Start-Sleep -Seconds 15
+            $sqlServerPrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $DbServerResourceIdForPrivateLink | Where-Object { $_.Name.StartsWith('ImportExportPrivateLink_SQL') } | Where-Object {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
+        }
+
+        $counter = 1
+        $storagePrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $StorageResourceIdForPrivateLink | Where-Object {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
+        while (!$storagePrivateEndpointConnection)
+        {
+            if ($counter -ge 12)
+            {
+                throw "Azure Storage private link not available."
+            }
+            $counter += 1
+            Write-Host "Waiting for Storage private link connection info..."
+            Start-Sleep -Seconds 15
+            $storagePrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $StorageResourceIdForPrivateLink | Where-Object {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
+        }
+
+        if ($sqlServerPrivateEndpointConnection)
+        {
+            Write-Host "Accepting SQL Server private link..."
+            Approve-AzPrivateEndpointConnection -ResourceId $sqlServerPrivateEndpointConnection.Id
+        }
+
+        if ($storagePrivateEndpointConnection)
+        {
+            Write-Host "Accepting Storage private link..."
+            Approve-AzPrivateEndpointConnection -ResourceId $storagePrivateEndpointConnection.Id
+        }
+    }    
+}
+
+function Wait-SingleAzureSqlDatabaseExportCompletion
+{
+    param (
+        [Microsoft.Azure.Commands.Sql.ImportExport.Model.AzureSqlDatabaseImportExportBaseModel] $exportRequest
+    )
+
+    $iterations = $waitSingleAzureSqlDatabaseExportCompletionMinutes * 60 / $sleepDurationSeconds
+
+    $counter = 0
+    do
+    {
+        if ($counter -ge $iterations)
+        {
+            Write-Warning "Timeout reached during database $($exportRequest.DatabaseName) export."
+            break
+        }
+
+        $counter += 1
+
+        Start-Sleep -Second $sleepDurationSeconds
+        $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink
+        
+        if ($exportRequestStatus.ErrorMessage)
+        {
+            throw [RetryException]::new($exportStatus.ErrorMessage)
+        }
+        Write-Host "  $($counter): Database $($database.DatabaseName) export status is $($exportRequestStatus.Status) ($($exportRequestStatus.StatusMessage))."                        
+    } while ($exportRequestStatus.Status -eq "InProgress")
+}
+
+class RetryException : System.Exception
+{
+    RetryException($Message) : base($Message)
+    {
+    }
 }
