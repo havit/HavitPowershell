a728d7f982c090cd9c0bc026feea1a3a31d5e1c8
Jiří Kanda <kanda@havit.cz>
2025-10-03 18:09:34 +0200

    HavitAzureSqlDatabasesExport - opravy, odkumentace

--- HavitAzureSqlDatabasesExport/HavitAzureSqlDatabasesExportCore.psm1
+++ HavitAzureSqlDatabasesExport/HavitAzureSqlDatabasesExportCore.psm1
@@ -67,29 +67,40 @@ function Export-SingleAzureSqlDatabase
 
     try
     {
+        # spustíme export databáze
+        # pokud se spuštění nepodaří (protože již běží, atp., je vrácena null hodnota)
+        # pokud se spuštění nepodaří z jiného důvodu, je vyhozena výjimka
         $exportRequest = Start-SingleAzureSqlDatabaseExport -database $database
-        # PRIVATE LINK ACCEPTANCE
-        Accept-SingleSqlDatabaseExportPrivateLinkEndpoints -database $database
+        
+        # Schválíme private endpointy (bez ohledu na konkrétní databázi)
+        # $exportRequest slouží k možnému čekání na založení infrastruktury je schválení
+        # Pokud byl ale spuštěn export dříve, nemáme $exportRequest, ale metoda se s tím musí vypořádat (schvaluje bez čekání) - to může být užitečné, pokud byl export spuštěn dříve a stále čeká na endpointy        
+        Accept-SingleSqlDatabaseExportPrivateLinkEndpoints -exportRequest $exportRequest
 
         # ELASTIC POOL - OVERLOAD PROTECTION
         # export databázi nemálo vytíží
         # tam, kde není elastic pool, s tím mnoho nenaděláme (můžeme povýšit), prostě musíme chvíli exportu přežít
         # tam, kde je elastic pool, můžeme *nezpůsobit* zátěž paralelní zálohou všech databází, čímž by měl zůstat výkový prostor pro běh aplikací        
-        if ($database.ElasticPoolName) # not null, not empty
-        {        
+        # čekat ale můžeme jen tehdy, pokud máme $exportRequest
+        # čas od času se při čekání dozvíme, že export selhal, pak je vyhozena RetryException, což řešíme novým čekáním a znovuspuštěním exportu
+        if ($exportRequest -and $database.ElasticPoolName) # not null, not empty
+        {
             Wait-SingleAzureSqlDatabaseExportCompletion -exportRequest $exportRequest
         }
     }
     catch [RetryException]
-    {        
+    {
+        # pokdu došlo k chybě, ze které se máme pokusit zotavit, počkáme chvilku a zkusíme export znovu (ale ne donekonečna)
         Write-Error $exception.Message
         Write-Host "Waiting 10 minutes..."
         Start-Sleep -Seconds 600 # 10 minutes
         if ($depth -lt 5) # max attempts per database
         {
+            Export-SingleAzureSqlDatabase -database $database -depth ($depth + 1)
         }
         else
         {
+            # pokud jsme se dostali za povolení množství znovuopakování, vyhodíme nakonec výjimku
             throw $_.Exception
         }
     }
@@ -98,7 +109,8 @@ function Export-SingleAzureSqlDatabase
 function Start-SingleAzureSqlDatabaseExport
 {
     param (
-        [Microsoft.Azure.Commands.Sql.Database.Model.AzureSqlDatabaseModel] $database
+        [Microsoft.Azure.Commands.Sql.Database.Model.AzureSqlDatabaseModel] $database,
+        [int] $depth = 0
     )
 
     $storageUri = "https://$StorageAccountName.blob.core.windows.net/$StorageContainerName/" # musí končit limítkem
@@ -119,30 +131,39 @@ function Start-SingleAzureSqlDatabaseExport
         SqlServerResourceIdForPrivateLink   = $DbServerResourceIdForPrivateLink
     }
 
-    Write-Host "Starting export $($database.DatabaseName) database to $bacpacName..."
-
-    $exportRequest = New-AzSqlDatabaseExport @exportRequestParams
-
-    if (-not $exportRequest)
-    {
-        Write-Warning "Failed."
-        Write-Host "Waiting 120 seconds..."
-        Start-Sleep -Second 120
-
-        Write-Host "Starting export $($database.DatabaseName) database to $bacpacName..."
-        $exportRequest = New-AzSqlDatabaseExport @exportRequestParams
-    }
-
-    if ($exportRequest)
-    {
-        Write-Host "Started export $($database.DatabaseName) database to $($exportRequest.StorageUri)."
-    }
-    else
+    # zastavení rekurze
+    if ($depth -eq 3)
     {
         throw "Starting export $($database.DatabaseName) failed."
     }
 
-    return $exportRequest
+    Write-Host "Starting export $($database.DatabaseName) database to $bacpacName..."
+
+    try
+    {
+        # Spustíme export databáze a vrátíme objekt reprezentující tento export
+        # ErrorAction: proměníme non-terminating errors na terminating (pro možnost zachycení catch)
+        $exportRequest = New-AzSqlDatabaseExport @exportRequestParams -ErrorAction Stop
+        Write-Host "Started export $($database.DatabaseName) database to $($exportRequest.StorageUri)."
+        return $exportRequest
+    }
+    catch
+    {
+        if ($_.Exception.Message.Contains('There is an import or export operation in progress on the database'))
+        {
+            Write-Warning $_.Exception.Message
+            Write-Host "Export $($database.DatabaseName) already started."
+            # pokud již import běží, nebudeme další spouštět
+            return $null
+        }
+
+        Write-Warning "Failed."
+        Write-Host "Waiting 120 seconds..."
+        Start-Sleep -Second 120
+
+        # v rekurzi zkusíme spustit export znovu
+        return Start-SingleAzureSqlDatabaseExport -database $database -depth ($depth + 1)
+    }
 }
 
 function Accept-SingleSqlDatabaseExportPrivateLinkEndpoints
@@ -153,23 +174,27 @@ function Accept-SingleSqlDatabaseExportPrivateLinkEndpoints
 
     if ($dbServerResourceIdForPrivateLink)
     {
-        $counter = 1
-        $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink                
-        while ($exportRequestStatus.PrivateEndpointRequestStatus.Count -eq 0)
+        # počkáme na založení infrastruktury
+        if ($exportRequest)
         {
-            if ($counter -ge 12)
+            $counter = 1
+            $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink                
+            while ($exportRequestStatus.PrivateEndpointRequestStatus.Count -eq 0)
             {
-                throw "Private link information not retrieved."
+                if ($counter -ge 12)
+                {
+                    throw "Private link information not retrieved."
+                }
+                $counter += 1
+                Write-Host "Waiting for private link connection info..."
+                Start-Sleep -Seconds 15
+                $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink
             }
-            $counter += 1
-            Write-Host "Waiting for private link connection info..."
-            Start-Sleep -Seconds 15
-            $exportRequestStatus = Get-AzSqlDatabaseImportExportStatus $exportRequest.OperationStatusLink
         }
 
         $counter = 1
         $sqlServerPrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $DbServerResourceIdForPrivateLink | Where-Object { $_.Name.StartsWith('ImportExportPrivateLink_SQL') } | Where-Object {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
-        while (!$sqlServerPrivateEndpointConnection)
+        while ((-not $sqlServerPrivateEndpointConnection) -and $exportRequest) #$exportRequest: opakujeme jen, pokud jsme právě spustili export databáze (dřívější exporty neopakujeme)        {
         {
             if ($counter -ge 12)
             {
@@ -183,7 +208,7 @@ function Accept-SingleSqlDatabaseExportPrivateLinkEndpoints
 
         $counter = 1
         $storagePrivateEndpointConnection = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $StorageResourceIdForPrivateLink | Where-Object {$_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
-        while (!$storagePrivateEndpointConnection)
+        while ((-not $storagePrivateEndpointConnection) -and $exportRequest)
         {
             if ($counter -ge 12)
             {
@@ -215,11 +240,12 @@ function Wait-SingleAzureSqlDatabaseExportCompletion
         [Microsoft.Azure.Commands.Sql.ImportExport.Model.AzureSqlDatabaseImportExportBaseModel] $exportRequest
     )
 
+    # kolikrát nejvýše můžeme zkoušet dokončení (s odstupem $sleepDurationSeconds sekund)
     $iterations = $waitSingleAzureSqlDatabaseExportCompletionMinutes * 60 / $sleepDurationSeconds
 
     $counter = 0
     do
-    {
+    {        
         if ($counter -ge $iterations)
         {
             Write-Warning "Timeout reached during database $($exportRequest.DatabaseName) export."
